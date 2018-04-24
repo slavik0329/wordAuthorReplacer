ff71f5408e2669aecf115a5ff6e8a358620ec65b
Steve Dakh <slavik0329@gmail.com>
2018-04-24 08:44:52 -0400

    convert to async/await

--- replacer.js
+++ replacer.js
@@ -1,11 +1,14 @@
-const zip = require('file-zip');
+const util = require("util");
+const unzip = util.promisify(require('file-zip').unzip);
 const replace = require("replace");
-const exec = require('child_process').exec;
+const exec = util.promisify(require('child_process').exec);
 
 const [, , file, fromName, toName] = process.argv;
 
-zip.unzip(file, 'output-6d7s', (err) => {
-  if (!err) {
+async function run() {
+  try {
+    await unzip(file, 'output-6d7s');
+
     replace({
       regex: fromName,
       replacement: toName,
@@ -13,15 +16,11 @@ zip.unzip(file, 'output-6d7s', (err) => {
       recursive: true
     });
 
-    exec("cd output-6d7s;zip -r output.docx ./*;cp output.docx ../", (err) => {
-      if (!err) {
-        exec("rm -rf ./output-6d7s");
-      } else {
-        console.log("Err", err)
-      }
-    });
-
-  } else {
-    console.log("Error: ", err);
+    await exec("cd output-6d7s;zip -r output.docx ./*;cp output.docx ../");
+    await exec("rm -rf ./output-6d7s");
+  } catch (e) {
+    console.log("Err", e)
   }
-});
+}
+
+run();
