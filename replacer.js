cda50d3d2df812c49042cf4f9461901affb36723
Steve Dakh <slavik0329@gmail.com>
2018-04-24 08:32:30 -0400

    cleared out unused changed output difectory to avoid collisions

--- replacer.js
+++ replacer.js
@@ -1,30 +1,24 @@
 const zip = require('file-zip');
 const replace = require("replace");
 const exec = require('child_process').exec;
-const fs = require('fs');
 
 const [, , file, fromName, toName] = process.argv;
 
-zip.unzip(file, 'output', (err) => {
+zip.unzip(file, 'output-6d7s', (err) => {
   if (!err) {
     replace({
       regex: fromName,
       replacement: toName,
-      paths: ["./output"],
+      paths: ["./output-6d7s"],
       recursive: true
     });
 
-    fs.readdir("./output", function (err, items) {
-      const formattedItems = items.map(item => "./output/" + item);
-      console.log("Zipping: ", formattedItems);
-
-      exec("cd output;zip -r output.docx ./*;cp output.docx ../", (err) => {
-        if (!err) {
-          exec("rm -rf ./output");
-        } else {
-          console.log("Err", err)
-        }
-      });
+    exec("cd output-6d7s;zip -r output.docx ./*;cp output.docx ../", (err) => {
+      if (!err) {
+        exec("rm -rf ./output-6d7s");
+      } else {
+        console.log("Err", err)
+      }
     });
 
   } else {
